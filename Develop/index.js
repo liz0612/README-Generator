c1cf9b557458e199a958592ff2b0d6c9f5663229
liz0612 <lizbeth.vaz0915@gmail.com>
2025-02-16 21:08:25 -0500

    Completed README Generator

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,66 @@
-// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a short description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'How do you install your project?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'How do you use your project?',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Select a license:',
+    choices: ['MIT', 'GPLv3', 'Apache 2.0', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'How can others contribute?',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'How do you run tests?',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email for questions:',
+  },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFileSync(fileName, data);
+  console.log('README.md has been generated!');
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const markdown = generateMarkdown(answers);
+    writeToFile('README.md', markdown);
+  });
+}
 
-// Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,68 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+    return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+  } else if (license === 'GPLv3') {
+    return '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+  } else if (license === 'Apache 2.0') {
+    return '![License](https://img.shields.io/badge/License-Apache_2.0-red.svg)';
+  }
+  return '';
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return '[MIT License](https://opensource.org/licenses/MIT)';
+  } else if (license === 'GPLv3') {
+    return '[GPLv3 License](https://www.gnu.org/licenses/gpl-3.0)';
+  } else if (license === 'Apache 2.0') {
+    return '[Apache 2.0 License](https://opensource.org/licenses/Apache-2.0)';
+  }
+  return '';
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return `## License
+    
+This project is licensed under the ${license} license.  
+More info: ${renderLicenseLink(license)}`;
+  }
+  return '';
+}
 
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+Find me on GitHub: [${data.github}](https://github.com/${data.github})  
+For additional questions, email me at: ${data.email}
 `;
 }
 
